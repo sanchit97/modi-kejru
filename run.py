48779bfc31889603613507cf75bc749a28778e37
sanchit97 <sanchit15083@iiitd.ac.in>
2017-12-27 01:13:25 +0530

    heroku

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+from app import app
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
